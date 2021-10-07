0b48bc3d8b472321474b84bffee826b7059f15bf
KevinLindemark <58036568+KevinLindemark@users.noreply.github.com>
2021-10-07 16:46:26 +0200

    MQTT bot

---
+++ mqttBotSimpel/credentials.py
@@ -0,0 +1,17 @@
+# python Dictionary {} der indeholder login oplysninger
+# (undlad at uploade den til github når den er udfyldt :)
+
+# ADAFRUIT_IO_URL -> behøves ikke at ændres
+# ADAFRUIT_IO_PUB_FEEDNAME -> behøves ikke at ændres - skal matche med feed navn du har oprettet på adafruit.io
+# ADAFRUIT_IO_SUB_FEEDNAME -> behøves ikke at ændres - skal matche med feed navn du har oprettet på adafruit.io
+
+credentials = {
+    'ssid' : 'navn på dit hotspot',
+    'password' : 'kode til hotspot',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'dit adafruit brugernavn',
+    'ADAFRUIT_IO_KEY' : b' din adafruit IO key',
+    'ADAFRUIT_IO_PUB_FEEDNAME' : b'bot_pub',
+    'ADAFRUIT_IO_SUB_FEEDNAME' : b'bot_sub'
+    }
+
